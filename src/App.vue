3e9589539950d6ed88918973ae75dfeab09becf3
Andrey <antuch.andrey@gmail.com>
2021-05-27 18:07:26 +0600

    Init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <NavBar></NavBar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+import NavBar from "@/components/NavBar.vue";
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component<App>({
+  components: {
+    NavBar,
+  },
+})
+export default class App extends Vue {}
+</script>
